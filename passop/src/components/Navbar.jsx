7b598ef02c0c8de466f1727d6d3f032d22121e9b
Azzam Abdul Khadar <azzamcse@gmail.com>
2025-10-07 09:28:00 +0530

    PassOP
    Designed a password saver application using react.js.

---
+++ passop/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-800 text-white">
+      <div className="mycontainer flex justify-between items-center px-4 h-14">
+        <div className="logo font-bold text-2xl ">
+          <span className="text-green-500">&lt;</span>Pass
+          <span className="text-green-500">OP/&gt;</span>
+        </div>
+        {/* <ul>
+          <li className="flex gap-4">
+            <a className="hover:font-bold " href="#">
+              Home
+            </a>
+            <a className="hover:font-bold " href="#">
+              About
+            </a>
+            <a className="hover:font-bold " href="#">
+              Contact
+            </a>
+          </li>
+        </ul> */}
+        <button className="flex items-center bg-green-700 p-1 rounded-full justify-between my-3">
+          <img className="invert px-2 w-12" src="icons/github.svg" alt="" />
+          <span className="font-bold text-lg">GitHub</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
